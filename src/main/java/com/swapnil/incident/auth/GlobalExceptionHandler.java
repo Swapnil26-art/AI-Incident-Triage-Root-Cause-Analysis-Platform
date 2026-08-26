8a9dd81cfab3b45baef8e35a8fc22f2a4d93a2d9
swapnil26-art <sp2120@srmist.edu.in>
2026-08-26 16:39:56 +0530

    fix: wrong import package for MethodArgumentNotValidException

--- src/main/java/com/swapnil/incident/auth/GlobalExceptionHandler.java
+++ src/main/java/com/swapnil/incident/auth/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -17,8 +18,8 @@ public class GlobalExceptionHandler {
         ));
     }
 
-    @ExceptionHandler(org.springframework.web.method.annotation.MethodArgumentNotValidException.class)
-    public ResponseEntity<?> handleValidation(org.springframework.web.method.annotation.MethodArgumentNotValidException e) {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidation(MethodArgumentNotValidException e) {
         String message = e.getBindingResult().getFieldErrors().stream()
                 .map(f -> f.getField() + ": " + f.getDefaultMessage())
                 .reduce((a, b) -> a + "; " + b)
